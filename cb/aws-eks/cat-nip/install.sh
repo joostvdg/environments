d23940469ba20d9350c5d92f58ea2268948eca52
Joost van der Griendt <joostvdg@gmail.com>
2018-10-15 11:27:44 +0200

    allow extra input for dry run scenarios

--- cb/aws-eks/cat-nip/install.sh
+++ cb/aws-eks/cat-nip/install.sh
@@ -1,3 +1,3 @@
 kubectl apply -f certificate.yml
-helm install --name cat-nip --namespace cat -f values.yml -f image-values.yml chartmuseum/cat-nip
+helm install --name cat-nip --namespace cat -f values.yml -f image-values.yml chartmuseum/cat-nip "$@"
 
